c538aa922024a42d4aebd410ef27752f0824bb56
Fratean Razvan <frateanrazvan@yahoo.com>
2024-11-06 21:01:58 +0200

    fix login, add navigation, fixes wip

--- src/app/authCallWrapper.tsx
+++ src/app/authCallWrapper.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import firebase from '@/service/client/firebase';
 import { getCookie } from '@/session/client-session';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { setUser } from '@/store/reducers/auth-slice';
@@ -36,37 +35,7 @@ export const ApiCallWrapper = ({
             },
           }),
         );
-      } else {
-        // No session
-        firebase.auth().onAuthStateChanged(userResp => {
-          if (userResp) {
-            // User is signed in
-            console.log('User is signed in: ', userResp, userStore);
-
-            // dispatch(
-            //   setUser({
-            //     user: {
-            //       uid: userResp.uid,
-            //       email: userStore.email,
-            //       userName: userStore.email.split('@')[0],
-            //       refreshToken: userResp.refreshToken,
-            //       newAccount: userStore.newAccount,
-            //     },
-            //   }),
-            // );
-          } else {
-            console.log(
-              'User is signed out: ',
-              userResp,
-              'userStore: ',
-              userStore,
-            );
-          }
-        });
       }
-      console.log('Cookie: ', cookie);
-    } else {
-      // already have user in store
     }
   }, [dispatch, userStore]);
 
