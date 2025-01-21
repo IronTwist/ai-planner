f34e6c3fbf98b000d1474a6ebe4c7c8a8822ed25
Fratean Razvan <frateanrazvan@yahoo.com>
2025-01-21 09:56:24 +0200

    fix sound generator

--- src/modules/UI/components/sound-generator.tsx
+++ src/modules/UI/components/sound-generator.tsx
@@ -1,22 +1,35 @@
+'use client';
+
 import { HemiSync } from '@/modules/common/soundGenerator';
 
 export const SoundGenerator = () => {
-    const hemy = new HemiSync(200, 7);
+  const hemy = new HemiSync(200, 7);
 
-    return (
-        <>
-  
-        <input
-        type="range"
-        id="volume"
-        min="0"
-        max="100"
-        step="1"
-        onChange={(e) => hemy.setVolume(e.target.valueAsNumber)}
-        />
-        <button onClick={() => hemy.play()}>Play</button>
-        <button onClick={() => hemy.stop()}>Stop</button>
-      </>
-      )
+  return (
+    <div
+      style={{
+        display: 'flex',
+        border: '1px solid white',
+        margin: '2px',
+        position: 'absolute',
+        top: 'auto',
+        bottom: '10px',
+        left: '100px',
+        gap: '20px',
+        padding: '5px',
+      }}
+    >
+      <div>Sound generator</div>
+      <input
+        type='range'
+        id='volume'
+        min='0'
+        max='100'
+        step='1'
+        onChange={e => hemy.setVolume(e.target.valueAsNumber)}
+      />
+      <button onClick={() => hemy.play()}>Play</button>
+      <button onClick={() => hemy.stop()}>Stop</button>
+    </div>
+  );
 };
-
--- src/modules/common/soundGenerator.js
+++ src/modules/common/soundGenerator.js
@@ -1,71 +1,72 @@
+'use client';
 
 export class HemiSync {
-    constructor(baseFrequency, beatFrequency) {
-      this.baseFrequency = baseFrequency;
-      this.beatFrequency = beatFrequency;
-      this.audioContext = new (window.AudioContext)();
-      this.leftOscillator = null;
-      this.rightOscillator = null;
+  constructor(baseFrequency, beatFrequency) {
+    this.baseFrequency = baseFrequency;
+    this.beatFrequency = beatFrequency;
+    this.audioContext = new window.AudioContext();
+    this.leftOscillator = null;
+    this.rightOscillator = null;
 
-      this.leftPanner = this.audioContext.createStereoPanner();
-      this.rightPanner = this.audioContext.createStereoPanner();
+    this.leftPanner = this.audioContext.createStereoPanner();
+    this.rightPanner = this.audioContext.createStereoPanner();
 
-      this.leftGain = this.audioContext.createGain();
-      this.rightGain = this.audioContext.createGain();
+    this.leftGain = this.audioContext.createGain();
+    this.rightGain = this.audioContext.createGain();
 
-      this.leftPanner.pan.value = -1; // Left channel
-      this.rightPanner.pan.value = 1; // Right channel
+    this.leftPanner.pan.value = -1; // Left channel
+    this.rightPanner.pan.value = 1; // Right channel
 
-      this.leftPanner.connect(this.leftGain);
-      this.rightPanner.connect(this.rightGain);
+    this.leftPanner.connect(this.leftGain);
+    this.rightPanner.connect(this.rightGain);
 
-      this.leftGain.connect(this.audioContext.destination);
-      this.rightGain.connect(this.audioContext.destination);
+    this.leftGain.connect(this.audioContext.destination);
+    this.rightGain.connect(this.audioContext.destination);
 
-      // Set default volume level (range: 0.0 to 1.0)
-      this.leftGain.gain.value = 0.5;
-      this.rightGain.gain.value = 0.5;
-    }
-
-    play() {
-      this.leftOscillator = this.audioContext.createOscillator();
-      this.rightOscillator = this.audioContext.createOscillator();
-
-      this.leftOscillator.connect(this.leftPanner);
-      this.rightOscillator.connect(this.rightPanner);
-
-      this.leftOscillator.frequency.value = this.baseFrequency;
-      this.rightOscillator.frequency.value =
-        this.baseFrequency + this.beatFrequency;
-
-      this.leftOscillator.start();
-      this.rightOscillator.start();
-    }
-
-    stop() {
-      this.leftOscillator.stop();
-      this.rightOscillator.stop();
-    }
-
-    setBaseFrequency(frequency) {
-      this.baseFrequency = frequency;
-    }
-
-    setBeatFrequency(frequency) {
-      this.beatFrequency = frequency;
-    }
-
-    setVolume(volume) {
-      // Clamp volume between 1 and 100
-      const clampedVolume = Math.max(1, Math.min(100, volume));
-      const step = 1;
-      const value = Math.floor(clampedVolume / step) * step;
-      this.leftGain.gain.value = value > 1 ? value / 100 : 0;
-      this.rightGain.gain.value = value > 1 ? value / 100 : 0;
-    }
+    // Set default volume level (range: 0.0 to 1.0)
+    this.leftGain.gain.value = 0.5;
+    this.rightGain.gain.value = 0.5;
   }
 
-  let hemy = new HemiSync(200, 7);
+  play() {
+    this.leftOscillator = this.audioContext.createOscillator();
+    this.rightOscillator = this.audioContext.createOscillator();
+
+    this.leftOscillator.connect(this.leftPanner);
+    this.rightOscillator.connect(this.rightPanner);
+
+    this.leftOscillator.frequency.value = this.baseFrequency;
+    this.rightOscillator.frequency.value =
+      this.baseFrequency + this.beatFrequency;
+
+    this.leftOscillator.start();
+    this.rightOscillator.start();
+  }
+
+  stop() {
+    this.leftOscillator.stop();
+    this.rightOscillator.stop();
+  }
+
+  setBaseFrequency(frequency) {
+    this.baseFrequency = frequency;
+  }
+
+  setBeatFrequency(frequency) {
+    this.beatFrequency = frequency;
+  }
+
+  setVolume(volume) {
+    // Clamp volume between 1 and 100
+    const clampedVolume = Math.max(1, Math.min(100, volume));
+    const step = 1;
+    const value = Math.floor(clampedVolume / step) * step;
+    this.leftGain.gain.value = value > 1 ? value / 100 : 0;
+    this.rightGain.gain.value = value > 1 ? value / 100 : 0;
+  }
+}
+
+// let hemy = new HemiSync(200, 7);
 
 //   function setBaseFrequency() {
 //     const baseFrequency = document.getElementById("baseFrequency").value;
@@ -77,10 +78,10 @@ export class HemiSync {
 //     hemy.setBeatFrequency(beatFrequency);
 //   }
 
-  export const startBinauralBeat = () => {
-    hemy.play();
-  }
+// export const startBinauralBeat = () => {
+//   hemy.play();
+// }
 
-  export const stopBinauralBeat = () => {
-    hemy.stop();
-  }
+// export const stopBinauralBeat = () => {
+//   hemy.stop();
+// }
